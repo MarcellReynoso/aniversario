440fea02993ac77320de9ca2f0e73f88487e600a
Marcell Alessandro Reynoso Flores <marcell.reynoso.flores@gmail.com>
2025-03-28 17:42:14 -0500

    Ajuste de tamaño

--- functions.js
+++ functions.js
@@ -1,20 +1,20 @@
-let titulo = document.createElement('h1');
-titulo.innerText = 'Tengo un mensajito para ti mi amor 💘';
+let titulo = document.createElement("h1");
+titulo.innerText = "Tengo un mensajito para ti mi amor 💘";
 document.getElementById("titulo").append(titulo);
 
-let imagen = document.createElement('img');
-imagen.src = 'Pompompurin.png';
+let imagen = document.createElement("img");
+imagen.src = "Pompompurin.png";
 document.getElementById("principal").append(imagen);
 
-let boton = document.createElement('button');
-boton.innerText = 'Haz click aquí para una sorpresita owo';
+let boton = document.createElement("button");
+boton.innerText = "Presiona aquí para ver una sorpresita owo";
 document.getElementById("principal").append(boton);
 
-boton.addEventListener('click', () => {
-    imagen.src = 'macarron.png';
-    document.getElementById("principal").append(imagen);
-    titulo.innerText = 'Eres lo mejor que me ha pasado y solo quiero decirte que te quiero mucho y estoy muy orgulloso de todas las cosas que haces, soy un hombre feliz de tenerte a mi lado. ❤️';
-    titulo.style= 'font-size: 35px; text-align: justify; font-weight: 300'
-    boton.style.display = 'none';
-    
-})
+boton.addEventListener("click", () => {
+  imagen.src = "macarron.png";
+  document.getElementById("principal").append(imagen);
+  titulo.innerText =
+    "Eres lo mejor que me ha pasado y solo quiero decirte que te quiero mucho y estoy muy orgulloso de todas las cosas que haces, soy un hombre feliz de tenerte a mi lado. ❤️";
+  titulo.style = "font-size: 35px; text-align: justify; font-weight: 300";
+  boton.style.display = "none";
+});
