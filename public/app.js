6bb6a7802aafb2c627b8f40ce8074be4d4fdf0bc
Marcell Alessandro Reynoso Flores <marcell.reynoso.flores@gmail.com>
2025-03-29 11:33:19 -0500

    Reestructuración de carpetas + ajuste de tamaño

---
+++ public/app.js
@@ -0,0 +1,20 @@
+let titulo = document.createElement("h1");
+titulo.innerText = "Tengo un mensajito para ti mi amor 💘";
+document.getElementById("titulo").append(titulo);
+
+let imagen = document.createElement("img");
+imagen.src = "src/assets/Pompompurin.png";
+document.getElementById("principal").append(imagen);
+
+let boton = document.createElement("button");
+boton.innerText = "Presiona aquí para ver una sorpresita owo";
+document.getElementById("principal").append(boton);
+
+boton.addEventListener("click", () => {
+  imagen.src = "src/assets/macarron.png";
+  document.getElementById("principal").append(imagen);
+  titulo.innerText =
+    "Eres lo mejor que me ha pasado y solo quiero decirte que te quiero mucho y estoy muy orgulloso de todas las cosas que haces, soy un hombre feliz de tenerte a mi lado. ❤️";
+  titulo.style = "font-size: 35px; text-align: justify; font-weight: 300";
+  boton.style.display = "none";
+});
